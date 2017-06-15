e1dedbd9e5d76dd57b2eb0494bfda01dbb4b7f0b
nmsz97 <raquaza.masters@gmail.com>
2017-06-15 15:57:01 -0600

    first commit

--- index.js
+++ index.js
@@ -1,67 +1,16 @@
-var _ = require("underscore"),
-    child_process = require("child_process");
-
-function init(path) {
-  var obj = new R(path);
-  return _.bindAll(obj, "data", "call", "callSync");
-}
-
-function R(path) {
-  this.d = {};
-  this.path = path;
-  this.options = {
-    env: _.extend({DIRNAME: __dirname}, process.env),
-    encoding: "utf8"
-  };
-  this.idCounter = 0;
-  this.args = ["--vanilla", __dirname + "/R/launch.R"];
-}
-
-R.prototype.data = function() {
-  for (var i = 0; i < arguments.length; i++) {
-    this.d[++this.idCounter] = arguments[i];
+function ordinal (number) {
+  var j = number % 10
+  var k = number % 100
+  if (j === 1 && k !== 11) {
+    return number + 'st'
   }
-  return this;
-};
+  if (j === 2 && k !== 12) {
+    return number + 'nd'
+  }
+  if (j === 3 && k !== 13) {
+    return number + 'rd'
+  }
+  return number + 'th'
+}
 
-R.prototype.call = function(_opts, _callback) {
-  var callback = _callback || _opts;
-  var opts = _.isFunction(_opts) ? {} : _opts;
-  this.options.env.input = JSON.stringify([this.d, this.path, opts]);
-  var child = child_process.spawn("Rscript", this.args, this.options);
-  child.stderr.on("data", callback);
-  child.stdout.on("data", function(d) {
-    callback(null, JSON.parse(d));
-  });
-};
-
-R.prototype.callSync = function(_opts) {
-  var opts = _opts || {};
-  this.options.env.input = JSON.stringify([this.d, this.path, opts]);
-  var child = child_process.spawnSync("Rscript", this.args, this.options);
-  if (child.stderr) throw child.stderr;
-  return(JSON.parse(child.stdout));
-};
-
-R.prototype.callSyncWithoutParse = function(_opts) {
-  var opts = _opts || {};
-  this.options.env.input = JSON.stringify([this.d, this.path, opts]);
-  var child = child_process.spawnSync("Rscript", this.args, this.options);
-  if (child.stderr) throw child.stderr;
-  return(child.stdout);
-};
-
-
-
-R.prototype.callWithoutParse = function(_opts, _callback) {
-  var callback = _callback || _opts;
-  var opts = _.isFunction(_opts) ? {} : _opts;
-  this.options.env.input = JSON.stringify([this.d, this.path, opts]);
-  var child = child_process.spawn("Rscript", this.args, this.options);
-  child.stderr.on("data", callback);
-  child.stdout.on("data", function(d) {
-    callback(null, d);
-  });
-};
-
-module.exports = init;
+module.exports = ordinal;
